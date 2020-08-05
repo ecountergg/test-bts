769e48dc1698ba7e08bbc05af15d20ad8e0e7663
ecountergg <ecountergg@gmail.com>
2020-08-05 11:39:04 +0700

    refactor App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  <div id="app" class="bg-primary">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
