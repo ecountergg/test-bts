6947d142f0f073474b12688c8208cccae48286d6
ecountergg <ecountergg@gmail.com>
2020-08-05 12:49:24 +0700

    refactor setup vuex and vue router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import Vuex from 'vuex'
 
-Vue.use(VueRouter, Vuex);
+import routes from '@/routes'
+import store from '@/store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
+  router: routes,
+  store,
   render: h => h(App),
-}).$mount('#app')
+})
