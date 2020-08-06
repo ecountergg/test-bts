e203db60c361548c4c555ca40be4c427f30b13a0
ecountergg <ecountergg@gmail.com>
2020-08-06 16:18:38 +0700

    setup admin panel

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div class="leading-normal tracking-normal" id="main-body">
+    <div class="flex flex-wrap">
+
+      <Sidebar />
+
+      <div class="w-full bg-gray-100 pl-0 lg:pl-64 min-h-screen" :class="{ 'overlay' : sideBarOpen }" id="main-content">
+
+        <Navbar />
+
+        <div class="p-6 bg-gray-100 mb-20">
+          <router-view />
+        </div>
+
+        <Footer />
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
+import Sidebar from '@/components/layout//Sidebar.vue'
+import Navbar from '@/components/layout//Navbar.vue'
+import Footer from '@/components/layout/Footer.vue'
 
 export default {
   name: 'App',
-}
+  computed: {
+    ...mapState(['sideBarOpen'])
+  },
+  components: {
+    Sidebar,
+    Navbar,
+    Footer,
+  },
+};
 </script>
-
 <style>
 @import "css/tailwind.css";
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,23 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
-        count: 0,
+        sideBarOpen: false
     },
-})
+    getters: {
+        sideBarOpen: state => {
+            return state.sideBarOpen;
+        },
+    },
+    mutations: {
+        toggleSidebar (state) {
+            state.sideBarOpen = !state.sideBarOpen;
+        },
+    },
+    actions: {
+        toggleSidebar(context) {
+            context.commit('toggleSidebar');
+        },
+    }
+});
 
 export default store;
