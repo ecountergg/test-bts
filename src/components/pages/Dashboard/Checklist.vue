c6f242e146dd2c01c03d475c7d66ba33599d6414
ecountergg <ecountergg@gmail.com>
2020-08-31 16:34:17 +0700

    create checklist

--- src/components/pages/Dashboard/Checklist.vue
+++ src/components/pages/Dashboard/Checklist.vue
@@ -1,11 +1,60 @@
 <template>
-    <div>
-        Checklist
+    <div class="w-full max-w-lg">
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3 mb-6 md:mb-0">
+                <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="grid-first-name"
+                >
+                    Checklist
+                </label>
+                <input
+                    id="grid-first-name"
+                    v-model="form.name"
+                    type="text"
+                    class="
+                        appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4
+                        mb-3 leading-tight focus:outline-none focus:bg-white
+                    "
+                    placeholder="Sample"
+                >
+            </div>
+        </div>
+        <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            @click="save"
+        >
+            Button
+        </button>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'Checklist',
+    data() {
+        return {
+            form: {
+                name,
+            },
+        };
+    },
+    methods: {
+        save() {
+            axios.post(`${process.env.VUE_APP_BASE_API_URL}checklist`, this.form)
+            .then((response) => {
+                console.log(response);
+                this.removeForm();  
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        },
+        removeForm() {
+            this.form.name = '';
+        },
+    },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import axios from 'axios'
 
 import routes from '@/routes'
 import store from '@/store'
@@ -7,6 +8,10 @@ import "@/css/tailwind.css";
 
 import App from './App.vue'
 
+const token = localStorage.getItem('token')
+if (token) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
 
 Vue.config.productionTip = false
 
